0d6c6f4e8bc34a947cba30d60988fd37b9dfd031
Jonatha Fernandes <jonathalfc@gmail.com>
2016-11-20 18:07:50 -0200

    Leia dois valores inteiros. A seguir, calcule o produto entre estes dois valores e atribua esta operação à variável PROD. A seguir mostre a variável PROD com mensagem correspondente.

---
+++ 1004_produto_simples.py
@@ -0,0 +1,11 @@
+
+def calcula_produto(numero1, numero2):
+		
+	PROD = int(numero1 * numero2)
+	return print('PROD = %d' %PROD)
+
+numero1 = int(input())
+numero2 = int(input())
+
+
+calcula_produto(numero1, numero2)
